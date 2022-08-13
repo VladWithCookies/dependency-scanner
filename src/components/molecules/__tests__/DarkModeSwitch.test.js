2061e0a85b2d6513814363c7cea8f40a7c447248
Vlad Viderko <summervibeswith9pm@gmail.com>
2022-08-13 16:43:03 +0200

    Add tests for theme switcher

---
+++ src/components/molecules/__tests__/DarkModeSwitch.test.js
@@ -0,0 +1,71 @@
+import { render, waitFor } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+
+import DarkModeSwitch from '../DarkModeSwitch.vue';
+
+describe('DarkModeSwitch', () => {
+  const add = vi.fn();
+  const toggle = vi.fn();
+
+  vi.spyOn(document, 'getElementsByTagName').mockReturnValue([
+    {
+      classList: {
+        add,
+        toggle,
+      },
+    },
+  ]);
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('when user turns on dark mode', () => {
+    it('adds correct class to html element', async () => {
+      const { container } = render(DarkModeSwitch);
+
+      userEvent.click(container.querySelector('input[type="checkbox"]'));
+
+      await waitFor(() => {
+        expect(toggle).toBeCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(toggle).toBeCalledWith('dark');
+
+      });
+    });
+  });
+
+  describe('when user turns off dark mode', () => {
+    it('removes correct class from html element', async () => {
+      const { container } = render(DarkModeSwitch);
+
+      userEvent.click(container.querySelector('input[type="checkbox"]'));
+
+      await waitFor(() => {
+        expect(toggle).toBeCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(toggle).toBeCalledWith('dark');
+      });
+    });
+  });
+
+  describe('when user has theme settings in local storage', () => {
+    it('adds correct class to html element', async () => {
+      vi.spyOn(Storage.prototype, 'getItem').mockReturnValueOnce('dark');
+
+      render(DarkModeSwitch);
+
+      await waitFor(() => {
+        expect(add).toBeCalledTimes(1);
+      });
+
+      await waitFor(() => {
+        expect(add).toBeCalledWith('dark');
+      });
+    });
+  });
+});
